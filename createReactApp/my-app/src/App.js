c16ee8c8a6d655d4566b8e1343da1c073b214abe
Sean Cahill <seancahill25@gmail.com>
2018-08-23 17:15:06 +0000

    added components and props

--- createReactApp/my-app/src/App.js
+++ createReactApp/my-app/src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import Ninjas from './ninjas';
 
 class App extends Component {
+  state = {
+    ninjas: [
+      {name: 'Sean' , age: 26, belt: 'black', id: 1},
+      {name: 'Shawn' , age: 32, belt: 'green', id: 2},
+      {name: 'Shaun' , age: 19, belt: 'purple', id: 3}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>My first React app!</h1>
         <p>Welcome :)</p>
+        <Ninjas ninjas={this.state.ninjas}/>
+
       </div>
     );
   }
